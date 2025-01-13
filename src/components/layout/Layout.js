c3743d89e0a99ecc0a3d053f1f8c91d6d83d7b51
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-14 03:03:16 +0330

    Add profile SVG icon and update layout to include user login/register section

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -9,8 +9,26 @@ function Layout({ children }) {
   return (
     <>
       <header>
-        <div className=" h-20 md:bg-red-800">
-          <div className="container mx-auto flex justify-between items-center h-full p-3">
+        <div className=" h-20">
+          <div className="container mx-auto flex justify-between items-center h-full p-10">
+            <div className="hidden md:flex items-center ">
+              <Image
+                src="/images/logo.png"
+                width={100}
+                height={100}
+                alt="logo"
+              ></Image>
+              <ul className="flex items-center  mr-9">
+                <li className="m-4">صفحه اصلی</li>
+                <li className="m-4">خدمات گردشگری</li>
+                <li className="m-4">درباره ما</li>
+                <li className="m-4">تماس با ما</li>
+              </ul>
+            </div>
+                      <div className="hidden md:flex items-center border border-green-500 rounded-lg p-2 cursor-pointer">
+                          <Image src="/images/profile.svg" width={20} height={20} alt="user"></Image>
+                            <span className="mr-1 text-green-600 text-md">ورود  |  ثبت نام</span>
+            </div>
             <div onClick={() => setShowMenu(!showMenu)} className="md:hidden">
               <Image
                 src="/images/menu.svg"

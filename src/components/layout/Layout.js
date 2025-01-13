1a4fca4256e2637f37e2a18620469831b1c8f5f8
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-13 22:09:44 +0330

    Update layout component styles and add cursor pointer to images

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -6,9 +6,9 @@ function Layout({ children }) {
     <>
       <header>
               <div className=" h-20 md:bg-red-800">
-                  <div className="container mx-auto flex justify-between items-center h-full p-2">
-                      <Image src="/images/signinbuttom.svg" width={40} height={40} alt="logo"></Image>
-                      <Image src="/images/menu.svg" width={20} height={20} alt="menu"></Image>
+                  <div className="container mx-auto flex justify-between items-center h-full p-3">
+                      <Image src="/images/signinbuttom.svg" width={40} height={40} alt="logo" className="cursor-pointer"></Image>
+                      <Image src="/images/menu.svg" width={20} height={20} alt="menu" className="cursor-pointer"></Image>
         </div>
         </div>
       </header>

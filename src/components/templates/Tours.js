4b35ad86f16866c25c4df64ebab127e85fa84cd6
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-16 21:44:18 +0330

    Add Cards component and integrate it into Tours for better tour display

--- src/components/templates/Tours.js
+++ src/components/templates/Tours.js
@@ -1,3 +1,5 @@
+import Cards from "../elements/Cards";
+
 export async function getTours() {
   const res = await fetch(`http://localhost:6500/tour`, { cache: "no-store" });
   const tours = await res.json();
@@ -7,7 +9,7 @@ export async function getTours() {
 async function Tours() {
   const tours = await getTours();
     return <div>{tours.map(tourList => (<ul>
-        <li key={tourList.id}>{tourList.title}</li>
+        <li key={tourList.id}><Cards tourList={tourList} /></li>
     </ul>)) }</div>;
 }
 

065cd8251bf84d27e0929b6688831b0a11f8156f
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-17 00:06:41 +0330

    Enhance Cards component to display tour price and add reservation link; improve layout and styling

--- src/components/elements/Cards.js
+++ src/components/elements/Cards.js
@@ -1,15 +1,29 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 
 function Cards({ tourList }) {
-    const {title , image ,options} = tourList
+    const {title , image ,options , price} = tourList
   return (
     <div className="flex justify-center items-center m-4">
-      <div className="w-auto   border border-[#0000001F] rounded-lg ">
+      <div className="w-auto   border border-[#0000001F] rounded-xl ">
         <div className="mx-auto w-[327.44px]  overflow-hidden">
           <img src={image} className="w-full overflow-hidden" />
-                  <h1 className="text-[20px] font-semibold m-3">{title}</h1>
-                  <span className='mr-3 text-sm text-primary-gray'>{`${options[0]} - ${options[1]} و ... `}</span>
+          <h1 className="text-[20px] font-semibold m-3">{title}</h1>
+          <span className="mr-3 text-sm text-primary-gray">{`${options[0]} - ${options[1]} و ... `}</span>
+        </div>
+        <div className="border-t border-t-slate-300">
+          <div className="flex justify-between items-center m-2 ">
+            <Link
+              href="#"
+              className="bg-primary-green p-1 w-[99px] text-center rounded-md text-white"
+            >
+              رزرو
+            </Link>
+            <p className='text-blue-500'>
+              {price} <spa className="text-primary-gray mr-1">تومان</spa>{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>

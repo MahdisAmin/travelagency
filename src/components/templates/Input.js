6d68f852b3654ae541e77bca0999c55a645d473c
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-16 20:01:38 +0330

    Refactor Input component layout and styles for improved user experience

--- src/components/templates/Input.js
+++ src/components/templates/Input.js
@@ -7,41 +7,61 @@ function Input() {
   const onSubmit = (data) => console.log(data);
   return (
     <div>
-      <div className="container p-10 mx-auto">
+      <div className="container p-10 mx-auto ">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto grid grid-cols-12 text-center gap-4 "
+          className="mx-auto grid grid-cols-12 text-center gap-3 md:border md:border-gray-200 rounded-lg p-1  md:max-w-[800px]"
         >
-          <div className="col-span-6 border md:col-span-3 border-gray-200 rounded-lg p-1 ">
-            <div className="flex items-center mr-3 ">
-              <Image src="/images/location.svg" width={18} height={18}></Image>
-              <input type="text" placeholder="مبدا" {...register("from")} className="mr-2" />
+          <div className=" col-span-6  md:col-span-3 border rounded-lg p-2 md:border-none ">
+            <div className="flex item-center w-full  ">
+              <Image
+                src="/images/location.svg"
+                width={18}
+                height={18}
+                className="md:mr-4"
+              ></Image>
+              <input
+                type="text"
+                placeholder="مبدا"
+                {...register("from")}
+                className="bg-transparent mr-2 outline-none"
+              />
             </div>
           </div>
-          <div className="col-span-6  md:col-span-3 border rounded-lg p-1 ">
-            <div className="flex item-center w-full mr-3 ">
-              <Image src="/images/to.svg" width={18} height={18}></Image>
+          <div className=" col-span-6  md:col-span-3 border rounded-lg p-2 md:border-none  ">
+            <div className="flex item-center w-full mr-3 md:border-r-2 md:border-gray-300  ">
+              <Image
+                src="/images/to.svg"
+                width={18}
+                height={18}
+                className="md:mr-4"
+              ></Image>
               <input
                 type="text"
                 placeholder="مقصد"
                 {...register("from")}
-                className="bg-transparent mr-2"
+                className="bg-transparent mr-2 outline-none"
               />
             </div>
           </div>
-          <div className="col-span-12 md:col-span-3  border border-gray-200  rounded-lg p-1 ">
-            <div className="flex items-center mr-3">
-              <Image src="/images/calendar.svg" width={18} height={18}></Image>
+          <div className=" col-span-12 md:col-span-3   border border-gray-200  rounded-lg p-2  md:border-none ">
+            <div className="flex items-center mr-3 md:border-r-2 md:border-gray-300  ">
+              <Image
+                src="/images/calendar.svg"
+                width={18}
+                height={18}
+                className="md:mr-4"
+              ></Image>
               <input
                 type="text"
                 placeholder="تاریخ"
                 {...register("from")}
-                className="mr-2 "
+                className="mr-2 outline-none "
               />
             </div>
           </div>
 
-          <div className="col-span-12 md:col-span-3 bg-primary-green rounded-lg p-1 text-center text-white">
+          <div className="  col-span-12 md:col-span-3 bg-primary-green rounded-lg p-1 text-center text-white ">
             <button type="submit">جستجو</button>
           </div>
         </form>

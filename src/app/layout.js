89842236601f56cc3cb048d6da93053677f3c040
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-18 23:27:40 +0330

    Update layout and enhance WhyTorino component with improved styling and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
-      <body className="font-yekanbakh">
+      <body className="font-yekanbakh max-w-[1440px]">
         <Layout />
         <div>
           <Image
--- src/components/templates/WhyTorino.js
+++ src/components/templates/WhyTorino.js
@@ -3,8 +3,8 @@ import React from "react";
 
 function WhyTorino() {
   return (
-    <div>
-      <div className="bg-gradient-to-r from-primary-green to-[#10411B] w-10 h-10 rounded-full mx-10 flex justify-center items-center">
+    <div className="flex items-center mr-11 md:mr-[80px]">
+      <div className="bg-gradient-to-r from-primary-green to-[#10411B] w-10 h-10 rounded-full  flex justify-center items-center">
         <Image
           src="/images/svgs/questionmark.svg"
           width={10}
@@ -12,6 +12,10 @@ function WhyTorino() {
           alt="questionMark"
         ></Image>
       </div>
+      <h1 className="font-extrabold text-3xl mr-2">
+        {" "}
+        چرا<span className="font-extrabold text-primary-green text-3xl mr-2"> تورینو</span> ؟
+      </h1>
     </div>
   );
 }

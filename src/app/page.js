2d02009e5441f505566e492b863b75962fb327ad
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-31 02:37:49 +0330

    Integrate Main component into Home for improved structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
-
+import Layout from "@/components/layout/Layout";
+import Main from "@/components/templates/Main";
 
 export default function Home() {
   return (
-    <div >
-    </div>
+    
+    <Main/>
   );
 }

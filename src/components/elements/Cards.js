f30edde61e5ac3912271389d801e9a9e7ae7e9a6
Mahdis Amin <mahdis.amin95@gmail.com>
2025-01-30 23:07:13 +0330

    Wrap Cards and Tours components in a container for improved layout consistency

--- src/components/elements/Cards.js
+++ src/components/elements/Cards.js
@@ -1,28 +1,30 @@
-import Image from 'next/image'
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Cards({ tourList }) {
-    const {title , image ,options , price} = tourList
+  const { title, image, options, price } = tourList;
   return (
-    <div className="flex justify-center items-center m-3">
-      <div className="w-auto   border border-[#0000001F] rounded-xl ">
-        <div className="mx-auto w-[300.44px]  overflow-hidden">
-          <img src={image} className="w-full overflow-hidden" />
-          <h1 className="text-[20px] font-semibold m-3">{title}</h1>
-          <span className="mr-3 text-sm text-primary-gray">{`${options[0]} - ${options[1]} و ... `}</span>
-        </div>
-        <div className="border-t border-t-slate-300">
-          <div className="flex justify-between items-center m-2 ">
-            <Link
-              href="#"
-              className="bg-primary-green p-1 w-[99px] text-center rounded-md text-white"
-            >
-              رزرو
-            </Link>
-            <p className='text-blue-500'>
-              {price} <spa className="text-primary-gray mr-1">تومان</spa>{" "}
-            </p>
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center m-3">
+        <div className="w-auto   border border-[#0000001F] rounded-xl ">
+          <div className="mx-auto w-[300.44px]  overflow-hidden">
+            <img src={image} className="w-full overflow-hidden" />
+            <h1 className="text-[20px] font-semibold m-3">{title}</h1>
+            <span className="mr-3 text-sm text-primary-gray">{`${options[0]} - ${options[1]} و ... `}</span>
+          </div>
+          <div className="border-t border-t-slate-300">
+            <div className="flex justify-between items-center m-2 ">
+              <Link
+                href="#"
+                className="bg-primary-green p-1 w-[99px] text-center rounded-md text-white"
+              >
+                رزرو
+              </Link>
+              <p className="text-blue-500">
+                {price} <spa className="text-primary-gray mr-1">تومان</spa>{" "}
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -30,4 +32,4 @@ function Cards({ tourList }) {
   );
 }
 
-export default Cards
+export default Cards;

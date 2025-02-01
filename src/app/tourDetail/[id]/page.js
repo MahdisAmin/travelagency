58d1e99c9cf2fad8642e99522ddab6c55904a87e
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-01 23:39:24 +0330

    Fetch and display tour details in TourDetails component

--- src/app/tourDetail/[id]/page.js
+++ src/app/tourDetail/[id]/page.js
@@ -1,7 +1,32 @@
+"use client";
+import Image from "next/image";
+
 import React from "react";
 
-function TourDetails() {
-  return <div>TourDetails</div>;
+export async function getTours(id) {
+  const res = await fetch(`http://localhost:6500/tour/${id}`, {
+    cache: "no-store",
+  });
+  const tours = await res.json();
+  return tours;
+}
+
+async function TourDetails({params}) {
+  const tours = await getTours(params.id);
+  const { id, title, image, options, price } = tours;
+  console.log(id)
+
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-center items-center m-3">
+       <img src={image} className="w-[330px] h-[220px] overflow-hidden" alt={title} />
+      </div>
+      <div>
+        <h1 className="text-[20px] font-semibold m-3">{title}</h1>
+         <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default TourDetails;

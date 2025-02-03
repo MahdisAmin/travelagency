20ea10ac2fe4298911c435a64b6f0120f3276173
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-04 01:41:03 +0330

    Enhance TourDetails component with improved styling for text elements and add booking button

--- src/app/tourDetail/[id]/page.js
+++ src/app/tourDetail/[id]/page.js
@@ -54,7 +54,9 @@ async function TourDetails({params}) {
             height={14}
             alt="usertick"
           ></Image>
-          <span className="mr-3 text-[#7D7D7D]">تورلیدر از مبدا</span>
+          <span className="mr-3 text-[#7D7D7D] text-[13px]">
+            تورلیدر از مبدا
+          </span>
         </div>
         <div className="flex justify-center items-center">
           <Image
@@ -63,7 +65,7 @@ async function TourDetails({params}) {
             height={14}
             alt="map"
           ></Image>
-          <span className="mr-3 text-[#7D7D7D]">برنامه سفر</span>
+          <span className="mr-3 text-[#7D7D7D] text-[13px]">برنامه سفر</span>
         </div>
         <div className="flex justify-center items-center">
           <Image
@@ -72,19 +74,21 @@ async function TourDetails({params}) {
             height={14}
             alt="medal"
           ></Image>
-          <span className="mr-3 text-[#7D7D7D]">تضمین کیفیت</span>
+          <span className="mr-3 text-[#7D7D7D] text-[13px]">تضمین کیفیت</span>
         </div>
       </div>
       <div className="flex justify-between items-center mx-16 my-7">
         <div>
-          <div className="flex  items-center ">
+          <div className="flex  items-center m-2">
             <Image src="/images/svgs/bus.svg" width={16} height={16}></Image>
-            <p className="mr-1 text-[#444444]">حمل و نقل</p>
+            <p className="mr-1 text-[#444444] ">حمل و نقل</p>
           </div>
-          <p className="text-center">{fleetVehicle}</p>
+          <p className="text-center font-semibold mt-3 text-sm">
+            {fleetVehicle}
+          </p>
         </div>
         <div>
-          <div className="flex  items-center ">
+          <div className="flex  items-center m-2 mr-6">
             <Image
               src="/images/svgs/twousers.svg"
               width={16}
@@ -92,28 +96,38 @@ async function TourDetails({params}) {
             ></Image>
             <p className="mr-1 text-[#444444]">ظرفیت</p>
           </div>
-          <p className="tracking-wider text-center">
+          <p className="tracking-wider mt-3 font-semibold text-sm text-center">
             {" "}
             حداکثر {toPersianNumber(availableSeats)} نفر
           </p>
         </div>
         <div>
-          <div className="flex  items-center ">
+          <div className="flex  items-center m-2 mr-9 ">
             <Image
               src="/images/svgs/security.svg"
               width={16}
               height={16}
             ></Image>
-            <p className="mr-1 text-[#444444]">بیمه</p>
+            <p className="mr-1  text-[#444444]">بیمه</p>
           </div>
           {insurance ? (
-            <p className="tracking-wider">
+            <p className="tracking-wider font-bold mt-3 text-sm mr-4">
               بیمه {toPersianNumber(50)} هزار دیناری
             </p>
           ) : (
-            <p>بیمه ندارد</p>
+            <p className="font-semibold mt-3 text-sm">بیمه ندارد</p>
           )}
         </div>
+      </div>
+      <div className="flex justify-between items-center mx-16 my-7">
+        <button className="bg-primary-green text-white  py-2 px-9 rounded-lg my-5">
+          {" "}
+          <a href={`/tourDetail/${id}/booking`}>رزرو و خرید</a>
+        </button>
+        <p className="text-2xl text-blue-500">
+          {toPersianNumber(price)}{" "}
+          <span className="text-sm text-primary-gray mr-3">تومان</span>
+        </p>
       </div>
     </div>
   );

a5eb2b9d550b8d67b0aab1cd0fa1523ba826ac00
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-09 23:57:55 +0330

    Refactor Auth modal to conditionally render based on visibility and enhance SendOtp styling for improved user experience

--- src/components/modal/Auth.js
+++ src/components/modal/Auth.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import SendOtp from './SendOtp'
 import CheckOtp from './CheckOtp'
 
-function Auth() {
+function Auth({ showModal, setShowModal }) {
+  if(!showModal)return
     const [step , setstep] = useState(1)
   return (
-    <div>
+    <div >
       {step === 1 && <SendOtp step={step} setstep={setstep} />}
       {step === 2 && <CheckOtp/>}
     </div>
--- src/components/modal/SendOtp.js
+++ src/components/modal/SendOtp.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 function SendOtp() {
   return (
-    <div className='flex items-center justify-center bg-gray-400 w-full blur-md'>
-      <div>
-
-      </div>
+    <div
+      className="flex items-center justify-center w-full h-full bg-[#00000080] blur-[1px] fixed top-0 left-0 z-[10000]"
+    >
+      <div className="w-[352px] h-[365px] rounded-lg bg-white p-5 relative "></div>
     </div>
-  )
+  );
 }
 
 export default SendOtp

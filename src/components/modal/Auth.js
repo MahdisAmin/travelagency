74ef6da49caea09a326773114597d388bf661270
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-09 23:07:46 +0330

    Add authentication modal with OTP verification steps

---
+++ src/components/modal/Auth.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import SendOtp from './SendOtp'
+import CheckOtp from './CheckOtp'
+
+function Auth() {
+    const [step , setstep] = useState(1)
+  return (
+    <div>
+      {step === 1 && <SendOtp step={step} setstep={setstep} />}
+      {step === 2 && <CheckOtp/>}
+    </div>
+  )
+}
+
+export default Auth
---
+++ src/components/modal/SendOtp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SendOtp() {
+  return (
+    <div className='flex items-center justify-center bg-gray-400 w-full blur-md'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default SendOtp

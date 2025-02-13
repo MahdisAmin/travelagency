8ceb5db2de6da48423918a3b71adadfef28bfb13
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-14 00:48:02 +0330

    Add checkOtp mutation for OTP verification and token management

--- src/config/services/mutations.js
+++ src/config/services/mutations.js
@@ -1,9 +1,22 @@
 import api from "../core/api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const sendOtp = () => {
   const mutationFn = (data) => api.post("auth/send-otp", data);
   return useMutation({ mutationFn });
 };
 
-export { sendOtp };
+const checkOtp = () => {
+  const queryClient = useQueryClient();
+
+  const mutationFn = (data) => api.post("auth/check-otp", data);
+
+  const onSuccess = (data) => {
+    setCookie("accessToken", data?.data?.accessToken, 30);
+    setCookie("refreshToken", data?.data?.refreshToken, 365);
+    queryClient.invalidateQueries({ queryKey: ["user-data"] });
+  };
+
+  return useMutation({ mutationFn, onSuccess });
+};
+export { sendOtp ,checkOtp };

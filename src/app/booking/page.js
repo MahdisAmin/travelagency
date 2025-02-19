db7bab0993d053b3a1ae72d197e9a9c83bd02ac2
Mahdis Amin <mahdis.amin95@gmail.com>
2025-02-20 01:36:56 +0330

    Add Booking component to handle search parameters

---
+++ src/app/booking/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Booking({ searchParams }) {
+ console.log(searchParams)
+    
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default Booking

35864370d59c8e84f0ab405a6b3b3817e91125a2
Mahdis Amin <mahdis.amin95@gmail.com>
2025-03-11 04:14:48 +0330

    Refactor UserInfo component layout for improved responsiveness and styling

--- src/components/elements/UserInfo.js
+++ src/components/elements/UserInfo.js
@@ -6,16 +6,21 @@ function UserInfo({ data }) {
     <div>
       <div className="p-5 border border-gray-300 rounded-lg">
         <p>اطلاعات حساب کاربری</p>
-        <div className="flex justify-between my-3 font-normal ">
-          <p className="text-gray-600 ">شماره موبایل</p>
-          <span>{toPersianNumber(+data?.data?.mobile)}</span>
-        </div>
-        <div className="flex justify-between my-3 font-normal">
-          <p className="">ایمیل -</p>
-          <button className="text-[#009ECA] flex">
-            <img src="/images/svgs/edit.svg" alt="edit"  className="ml-1"/>
-            افزودن
-          </button>
+        <div className="md:flex md:justify-between">
+          <div className="flex justify-between my-3 font-normal ">
+            <p className="text-gray-600 md:ml-6 ">شماره موبایل</p>
+            <span>{toPersianNumber(+data?.data?.mobile)}</span>
+          </div>
+          <div className="flex justify-between my-3 font-normal">
+            <div className="flex md:ml-20 lg:ml-80">
+              <p className="ml-7">ایمیل </p>
+              <span>-</span>
+            </div>
+            <button className="text-[#009ECA] flex">
+              <img src="/images/svgs/edit.svg" alt="edit" className="ml-1" />
+              افزودن
+            </button>
+          </div>
         </div>
       </div>
     </div>

48432ad2d47f33c1238682f2d4d4c1e0b1326ea2
Mahdis Amin <mahdis.amin95@gmail.com>
2025-03-17 02:00:56 +0330

    Add email editing functionality and validation in UserInfo component; create EmailForm component

--- src/components/elements/UserInfo.js
+++ src/components/elements/UserInfo.js
@@ -1,25 +1,98 @@
+"use client"
+import { useUpdatePersonalInfo } from "@/config/services/mutations";
 import { toPersianNumber } from "@/utils/extras";
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { isEmail } from "validator";
 
 function UserInfo({ data }) {
+  const { mutate: updateProfile } = useUpdatePersonalInfo();
+  const [isEditing, setIsEditing] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+   const previousEmail = data?.data?.email;
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+    setValue("email", data?.data?.email || "");
+  };
+
+  const onSubmit = (data) => {
+    if (!isEmail(data.email)) {
+      return;
+    }
+
+    updateProfile({ email: data.email });
+    setIsEditing(false);
+  };
   return (
     <div>
       <div className="p-5 border border-gray-300 rounded-lg">
         <p>اطلاعات حساب کاربری</p>
         <div className="md:flex md:justify-between">
-          <div className="flex justify-between my-3 font-normal ">
-            <p className="text-gray-600 md:ml-6 ">شماره موبایل</p>
+          <div className="flex justify-between my-3 font-normal">
+            <p className="text-gray-600 md:ml-6">شماره موبایل</p>
             <span>{toPersianNumber(+data?.data?.mobile)}</span>
           </div>
           <div className="flex justify-between my-3 font-normal">
             <div className="flex md:ml-20 lg:ml-80">
-              <p className="ml-7">ایمیل </p>
-              <span>-</span>
+              {!isEditing && (
+                <>
+                  <p className="ml-7">ایمیل</p>
+                  <span>{previousEmail || "-"}</span>{" "}
+                </>
+              )}
             </div>
-            <button className="text-[#009ECA] flex">
-              <img src="/images/svgs/edit.svg" alt="edit" className="ml-1" />
-              افزودن
-            </button>
+            {!isEditing && (
+              <button
+                onClick={handleEditClick}
+                className="text-[#009ECA] flex font-bold"
+              >
+                <img src="/images/svgs/edit.svg" alt="edit" className="ml-1" />
+                افزودن
+              </button>
+            )}
+            {isEditing && (
+              <div className="flex flex-col relative">
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="flex absolute left-28"
+                >
+                  <input
+                    type="email"
+                    {...register("email", {
+                      required: "ایمیل نمی‌تواند خالی باشد",
+                      validate: (value) =>
+                        isEmail(value) || "ایمیل وارد شده معتبر نیست",
+                    })}
+                    className="border border-gray-300 p-2 rounded mt-2"
+                    placeholder="ایمیل خود را وارد کنید"
+                  />
+                  {errors.email && (
+                    <span className="text-red-500 text-sm mt-1 absolute -bottom-5">
+                      {errors.email.message}
+                    </span>
+                  )}
+                  <button
+                    type="submit"
+                    className="bg-primary-green text-white px-4 py-1 mr-2 rounded-lg mt-3"
+                  >
+                    تایید
+                  </button>
+                </form>
+                <button
+                  onClick={() => setIsEditing(false)}
+                  className="text-gray-500 mt-3 border border-gray-200 py-1 px-5 rounded-lg"
+                >
+                  لغو
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>

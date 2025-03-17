b3710eeb46c33cc18548823bed11e217e6054b51
Mahdis Amin <mahdis.amin95@gmail.com>
2025-03-18 01:20:25 +0330

    Refactor BankAccountInfo component to include editing functionality and remove EmailForm component

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -3,6 +3,7 @@
 import BankacoountInfo from "@/components/elements/BankacoountInfo";
 import PersonalInfo from "@/components/elements/PersonalInfo";
 import UserInfo from "@/components/elements/UserInfo";
+import BankAccountForm from "@/components/templates/BankAccountForm";
 import { useGetUserData, useGetUserTours } from "@/config/services/query";
 import React from "react";
 
@@ -14,7 +15,7 @@ function UserProfile() {
     <div>
       <UserInfo data={data} refetch={refetch} />
       <PersonalInfo data={data} />
-      <BankacoountInfo/>
+      <BankacoountInfo data={data} />
     </div>
   );
 }
--- src/components/elements/BankacoountInfo.js
+++ src/components/elements/BankacoountInfo.js
@@ -1,35 +1,60 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import BankAccountForm from "../templates/BankAccountForm";
 
-function BankacoountInfo() {
-   return (
-     <div>
-       <div className="p-5 border border-gray-300 rounded-lg mt-3 font-normal">
-         <div className="flex justify-between font-bold">
-           <p>اطلاعات حساب بانکی</p>
-           <button className="text-[#009ECA] flex">
-             <img src="/images/svgs/edit.svg" alt="edit" className="ml-1" />
-             ویرایش اطلاعات
-           </button>
-         </div>
-         <div className="flex flex-col md:flex-row md:justify-between md:flex-wrap md:max-w-[700px] ">
-           <div className="flex justify-between my-3 ">
-             <p className="md:ml-2">شماره کارت</p>
-             <span className="font-semibold">-</span>
-           </div>
-           <div className="flex justify-between my-3 ">
-             <p className="md:ml-2">شماره شبا</p>
-             <span className="font-semibold">-</span>
-           </div>
-         </div>
-         <div className="flex flex-col md:flex-row md:justify-between md:flex-wrap md:max-w-[700px] ">
-           <div className="flex justify-between my-3 ">
-             <p className="md:ml-2">شماره حساب</p>
-             <span className="font-semibold">-</span>
-           </div>
-         </div>
-       </div>
-     </div>
-   );
+function BankacoountInfo({ data }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const editHandler = () => {
+    setIsEditing(true);
+  };
+  const handleCancel = () => {
+    setIsEditing(false);
+  };
+  console.log(data?.data);
+
+  return (
+    <div>
+      <div className="p-5 border border-gray-300 rounded-lg mt-3 font-normal">
+        {isEditing ? (
+          <BankAccountForm cancel={handleCancel} setIsEditing={setIsEditing} />
+        ) : (
+          <>
+            <div className="flex justify-between font-bold">
+              <p>اطلاعات حساب بانکی</p>
+              <button className="text-[#009ECA] flex" onClick={editHandler}>
+                <img src="/images/svgs/edit.svg" alt="edit" className="ml-1" />
+                ویرایش اطلاعات
+              </button>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between md:flex-wrap md:max-w-[700px] ">
+              <div className="flex justify-between my-3 ">
+                <p className="md:ml-2">شماره کارت</p>
+                <span className="font-semibold">
+                  {" "}
+                  {data?.data.payment?.debitCard_code || "-"}
+                </span>
+              </div>
+              <div className="flex justify-between my-3 ">
+                <p className="md:ml-2">شماره شبا</p>
+                <span className="font-semibold">
+                  {data?.data.payment?.shaba_code || "-"}
+                </span>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row md:justify-between md:flex-wrap md:max-w-[700px] ">
+              <div className="flex justify-between my-3 ">
+                <p className="md:ml-2">شماره حساب</p>
+                <span className="font-semibold">
+                  {" "}
+                  {data?.data.payment?.accountIdentifier || "-"}
+                </span>
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default BankacoountInfo
+export default BankacoountInfo;
